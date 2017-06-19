d88f211696b9a1e49ae8dfac96a78b6bdf0513a8
Edor <em.dortenzi@gmail.com>
2017-06-19 15:19:16 +0200

    Commit Docker 1.1

--- asw-progetto-master/build-all-images.sh
+++ asw-progetto-master/build-all-images.sh
@@ -5,13 +5,8 @@ source "docker.env"
 # DOCKER_REGISTRY=localhost:5000
 DOCKER_REGISTRY=swarm.inf.uniroma3.it:5000
 
-docker build --rm -t ${DOCKER_REGISTRY}/eureka-9007 ./eureka 
-docker build --rm -t ${DOCKER_REGISTRY}/S-9007 ./S
-docker build --rm -t ${DOCKER_REGISTRY}/S1-9007 ./S1
-docker build --rm -t ${DOCKER_REGISTRY}/S2-9007 ./S2
+docker build --rm -t ${DOCKER_REGISTRY}/eureka-9007 ./eureka
+docker build --rm -t ${DOCKER_REGISTRY}/s-9007 ./S
+docker build --rm -t ${DOCKER_REGISTRY}/s1-9007 ./S1
+docker build --rm -t ${DOCKER_REGISTRY}/s2-9007 ./S2
 docker build --rm -t ${DOCKER_REGISTRY}/zuul-9007 ./ZuulApplication
-
-
-
-
-
--- asw-progetto-master/push-all-images.sh
+++ asw-progetto-master/push-all-images.sh
@@ -6,9 +6,9 @@ source "docker.env"
 DOCKER_REGISTRY=swarm.inf.uniroma3.it:5000
 
 docker push ${DOCKER_REGISTRY}/eureka-9007
-docker push ${DOCKER_REGISTRY}/S-9007
-docker push ${DOCKER_REGISTRY}/S1-9007
-docker push ${DOCKER_REGISTRY}/S2-9007
+docker push ${DOCKER_REGISTRY}/s-9007
+docker push ${DOCKER_REGISTRY}/s1-9007
+docker push ${DOCKER_REGISTRY}/s2-9007
 docker push ${DOCKER_REGISTRY}/zuul-9007
 
 
